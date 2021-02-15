12413d725c27435c525860c1612cd07172c14261
sunny <parks38@uw.edu>
2021-02-15 15:37:03 +0900

    updated static content, MVC, Api examples

---
+++ hello-spring-g/src/test/java/hello/hellospringg/HelloSpringGApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellospringg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringGApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
